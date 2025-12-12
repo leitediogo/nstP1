f8a2c85335d7816044ac17acafc34e5bf5b57374
leitediogo <diogo.leite1@e1671247.lan>
2025-12-12 12:44:24 +0000

    initial comit

---
+++ testFile.js
@@ -0,0 +1,11 @@
+function concatenateStrings(str1, str2) {
+    return str1 + str2;
+}
+
+module.exports = concatenateStrings;
+// Example usage:
+// const concatenateStrings = require('./testFile');
+// console.log(concatenateStrings('Hello, ', 'World!')); // Output: Hello, World!       
+
+
+// To run this file, use the command: node testFile.js
